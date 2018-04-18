ec61c2460ba10637a1125e609fb82e06242a061e
RubenCameroAmador <37782994+RubenCameroAmador@users.noreply.github.com>
2018-04-18 17:19:30 -0500

    Initial commit

---
+++ src/tu/film/TuFilm.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tu.film;
+
+/**
+ *
+ * @author Adriana Arango
+ */
+public class TuFilm {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Administrador a= new Administrador();
+        a.setVisible(true);
+    }
+    
+}
